5266806259fcab80dc9d6f5eda1df0b7738d2ce7
apple <apple@apples-MacBook-Pro.local>
2026-03-19 18:48:34 +0800

    created table migrations as initialization

--- backend/bootstrap/cache/packages.php
+++ backend/bootstrap/cache/packages.php
@@ -17,6 +17,13 @@
       0 => 'Laravel\\Horizon\\HorizonServiceProvider',
     ),
   ),
+  'laravel/passport' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Passport\\PassportServiceProvider',
+    ),
+  ),
   'laravel/sail' => 
   array (
     'providers' => 
