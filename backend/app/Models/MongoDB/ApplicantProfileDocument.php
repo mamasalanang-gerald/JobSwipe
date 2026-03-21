741535c934da3442e99355c89c9f389cec38811a
apple <apple@apples-MacBook-Pro.local>
2026-03-21 20:49:36 +0800

    mongodb models

---
+++ backend/app/Models/MongoDB/ApplicantProfileDocument.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models\Mongo;
+
+use MongoDB\Laravel\Eloquent\Model;
+
+class ApplicantProfileDocument extends Model
+{
+    protected $connection = 'mongodb';
+    protected $collection = 'applicant_profiles';
+
+    protected $fillable = [
+        'user_id',
+        'first_name',
+        'last_name',
+        'profile_photo_url',
+        'bio',
+        'location',
+        'location_city',
+        'location_region',
+        'linkedin_url',
+        'social_links',
+        'resume_url',
+        'cover_letter_url',
+        'portfolio_url',
+        'skills',
+        'work_experience',
+        'education',
+        'completed_profile_fields',
+    ];
+
+    protected $casts = [
+        'skills' => 'array',
+        'work_experience' => 'array',
+        'education' => 'array',
+        'social_links' => 'array',
+        'completed_profile_fields' => 'array',
+    ];
+}
